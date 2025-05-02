7d15f8ae1342a6dca5fcfc5773358683ac60fe3e
Shibaditya Deb <debshibaditya144@gmail.com>
2025-05-02 14:48:27 +0530

    links changed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -15,7 +15,7 @@ function Footer() {
               their dreams and make a positive impact on the world.
             </p>
             <div className="social-icons">
-              <a href="#" className="social-icon">
+              <a href="https://www.facebook.com/shibadityadeb" className="social-icon">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="20"
@@ -30,7 +30,7 @@ function Footer() {
                   <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
                 </svg>
               </a>
-              <a href="#" className="social-icon">
+              <a href="https://www.instagram.com/shivaditya_dev?igsh=MW1mOGU3ejZoaWJpZg==" className="social-icon">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="20"
@@ -47,7 +47,7 @@ function Footer() {
                   <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
                 </svg>
               </a>
-              <a href="#" className="social-icon">
+              <a href="https://x.com/ShibadityaDeb" className="social-icon">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="20"
@@ -62,7 +62,7 @@ function Footer() {
                   <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
                 </svg>
               </a>
-              <a href="#" className="social-icon">
+              <a href="https://www.linkedin.com/in/shibadityadeb/" className="social-icon">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="20"
